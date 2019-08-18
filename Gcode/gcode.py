5035ed18706b0c3d6a91eac54a1cf9986faa0836
Chris Anderson <gca2@earthlink.net>
2019-08-17 23:30:50 -0700

    trimmed

--- Gcode/gcode.py
+++ Gcode/gcode.py
@@ -8,9 +8,8 @@ pairs = []
 with open('triangle.gcode', 'r') as fh:
         for line_text in fh.readlines():
                 line = Line(line_text)
-                line.block.gcodes  # is your list of gcodes
+                line.block.gcodes  # list of gcodes
                 if line.block.gcodes: # not a blank line
-                        line.block.modal_params  # are all parameters not assigned to a gcode, assumed to be motion modal parameters
                         (befores, (g,), afters) = split_gcodes(line.block.gcodes, (GCodeRapidMove, GCodeLinearMove))
                         if g.X is not None:
                             pairs = [[g.X],[g.Y]]
