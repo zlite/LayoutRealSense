26e916177aa00a5624add86d99192358c6e7f954
Chris Anderson <gca2@earthlink.net>
2019-08-17 23:20:35 -0700

    First commit of Gcode parser

---
+++ Gcode/gcode.py
@@ -0,0 +1,22 @@
+# requires PyGCode: pip3 install pygcode
+
+from pygcode import GCodeRapidMove, GCodeLinearMove
+from pygcode import Machine, Line, split_gcodes
+import numpy as np
+m = Machine()
+coordinates = []
+pairs = []
+i = 0
+with open('triangle.gcode', 'r') as fh:
+        for line_text in fh.readlines():
+                line = Line(line_text)
+                line.block.gcodes  # is your list of gcodes
+                if line.block.gcodes: # not a blank line
+                        line.block.modal_params  # are all parameters not assigned to a gcode, assumed to be motion modal parameters
+                        (befores, (g,), afters) = split_gcodes(line.block.gcodes, (GCodeRapidMove, GCodeLinearMove))
+                        if g.X is not None:
+                            pairs = [[g.X],[g.Y]]
+                            coordinates.append(pairs)
+                            i = i+1
+print("Number of points:", len(coordinates))
+print(coordinates)
